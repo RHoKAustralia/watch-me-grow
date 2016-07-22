bbc1ba9ae097c780d27767b55afd03fb91e008b0
Alex Gilleran <alex@alexgilleran.com>
2016-07-22 21:12:18 +1000

    Change logo.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Link} from 'react-router';
+import { Link } from 'react-router';
+import headerLogo from './wmg-header-logo.svg';
 
 import Styles from './header.scss';
 
@@ -7,7 +8,7 @@ const Header = React.createClass({
     render() {
         return (
             <header className={Styles.header}>
-                <span className={Styles.logo} />
+                <svg className={Styles.logo} dangerouslySetInnerHTML={{__html: headerLogo}} />
                 <h1 className={Styles.title}>Watch Me Grow</h1>
                 <Link className={Styles.newChild} to="landing" activeClassName={Styles.active}>New Child</Link>
             </header>
