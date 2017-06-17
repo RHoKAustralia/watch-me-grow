a0c59aa7d1741d107b1a0cb6a9db7073dd3a5cb1
Alex Gilleran <alex.gilleran@nicta.com.au>
2017-06-17 16:18:43 +1000

    Made lambda generate email.

--- lambda/mark-and-send/index.js
+++ lambda/mark-and-send/index.js
@@ -3,6 +3,7 @@
 let https = require("https");
 const fetch = require("isomorphic-fetch");
 var dataFunctions = require("wmg-common/data-functions");
+var markupJs = require("markup-js");
 var mark = dataFunctions.mark;
 var combineAll = dataFunctions.combineAll;
 var strings = require("wmg-common/strings");
@@ -28,19 +29,39 @@ exports.handler = function(event, context) {
     context.fail("Error: Missing parameter.");
   }
 
-  const combinedResults = combineAll(event.results);
-  const concern = mark(combinedResults);
-//   const resultStrings = concern
-//     ? strings.result.concerns
-//     : strings.result.noConcerns;
+  fs.readFile(__dirname + "/Results.html", "utf-8", (err, templateBody) => {
+    if (err) {
+      // Error
+      console.log(err, err.stack);
+      context.fail("Internal Error: Failed to load template.");
+    } else {
+      const combinedResults = combineAll(event.results);
+      const concern = mark(combinedResults);
+      const resultStrings = concern
+        ? strings.result.concerns
+        : strings.result.noConcerns;
 
-  
+      var message = markupJs.up(templateBody, {
+        details: event.details,
+        concern: concern,
+        allResults: combinedResults,
+        resultText: resultStrings.title + " " + resultStrings.subtitle
+      });
 
-  fetch("https://hooks.zapier.com/hooks/catch/2318292/9cdxwr/", {
-    method: "POST",
-    body: JSON.stringify(combinedResults),
-    headers: {
-      "Content-Type": "application/json"
+      const x = {
+        results: event.results,
+        concern: concern,
+        details: event.details,
+        message: message
+      };
+
+      fetch("https://hooks.zapier.com/hooks/catch/2318292/9cdxwr/", {
+        method: "POST",
+        body: JSON.stringify(x),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
     }
   });
 };
